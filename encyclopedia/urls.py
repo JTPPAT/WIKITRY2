727f12e791e45f94bbefa61679022abb08b56f8f
JTPPAT <jtp20061@icloud.com>
2020-07-10 08:17:48 -0700

    links for entrys working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("", views.{{entry}}, name="{{entry}}")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("/newpage", views.newpage, name="newpage"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    markdown = util.get_entry(entry)
+    return render(request, 'encyclopedia/title.html', {'content' : markdown}
+)
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html", {
+            })
+
