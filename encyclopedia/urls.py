1dc70b3922dcc99562a4fb0cd858f9d331124d01
JTPPAT <jtp20061@icloud.com>
2020-07-12 09:55:46 -0700

    try new page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("/newpage", views.newpage, name="newpage"),
+    
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -10,9 +10,29 @@ def index(request):
 
 def entry(request, entry):
     markdown = util.get_entry(entry)
-    return render(request, 'encyclopedia/title.html', {'content' : markdown}
+    markdown_code = util.get_entry(entry)
+    converted_code = markdown2.markdown(markdown_code)
+    return render(request, 'encyclopedia/title.html', {'content' : converted_code }
+    
 )
 def newpage(request):
-    return render(request, "encyclopedia/newpage.html", {
+    if request.method == "POST":
+        header = request.POST.get("header")
+        content = request.POST.get("content")
+        try = util.get_entry(header)
+        if test != None:
+            messages.error(request,"This page already exists")
+            return render(request, "encyclopedia/newpage.html", 
+            "entries": util.list_entries()
             })
 
+    else:
+        util.save_entry(header,content)
+        entry_new = util.get_entry(header)
+        return redirect("/wiki/"+title)
+else:
+    header = ""
+    content =""
+    return render(request, "encyclopedia/newpage.html", 
+            "entries": util.list_entries()
+            })
